ea4b9b4d6f4c451c436790658cf993126987acc5
korobchinskiy <45297659+korobchinskiy@users.noreply.github.com>
2018-11-24 00:43:25 +0500

    Create Sam.php

---
+++ Sam.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+   <meta charset="UTF-8">
+   <title>Тестируем php</title>
+   </head>
+  <body>
+    <h1>Welcome!</h1>
+
+ <?php echo '<p>Привет</p>'; ?>
+
+  </body>
+</html>
